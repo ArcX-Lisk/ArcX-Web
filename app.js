1e06dd0747976627b40a66ac0ba83cfcc3d40017
ArcX-Lisk <liskarcx@arcx.gg>
2024-05-10 09:41:21 +0800

    update
    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import router from './router/index.js'
 import bodyParser from 'body-parser'
 import WebSocket from 'ws'
 const app = express()
-// 初始化WebSocket服务器
+
 // const ws = new WebSocket('ws://3.90.29.127:8900')
 
 // ws.on('open', () => {
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/api/arcx_solana', router)
 
-// 设置 CORS 头部
+
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
@@ -50,7 +50,7 @@ app.use(function (req, res, next) {
 
 const PORT = 3007
 
-// 启动服务器
+
 app.listen(PORT, function () {
   console.log(`Server running at http://127.0.0.1:${PORT}/`)
 })
